cea177860f5ef3d0696678fcb506f084bf3a0a6f
Replit user <>
2024-01-17 03:59:07 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Polygon Area Calculator
-
 This is the boilerplate for the Polygon Area Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/polygon-area-calculator
+##
+### Message from Thiago Ferrari  
+This is my solution for freeCodeCamp.org's Polygon Area Calculator - Scientific Computing with Python Projects
+
+
+This boilerplate brings all the necessary files for running and testing the program.
+The only file I edited was the "shape_calculator.py" where I wrote the python program following the instructions stated on freeCodeCamp.org link mentioned above.  
+
+The file main.py imports the "shape_calculator.py" to be excecuted. It also starts the test_module.py - where I could check whether or not I met all of the project's requirements.
+##
+### Orinal Remote repository from freeCodeCamp
+**Remote:** https://github.com/freeCodeCamp/boilerplate-polygon-area-calculator  
+**Repository Name:** boilerplate-polygon-area-calculator  
+**Repository Description:** Repository for https://replit.com/@ferrarithiago/boilerplate-polygon-area-calculator  
+##
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,74 @@
-class Rectangle:
+class Rectangle():
+  def __init__(self, width, height):
+      self.width = width
+      self.height = height
 
 
+  def __str__(self):
+      return f"Rectangle(width={self.width}, height={self.height})"
 
 
-class Square:
+  def set_width(self, new_width):
+      self.width = new_width
+
+
+  def set_height(self, new_height):
+      self.height = new_height
+
+
+  def get_area(self):
+      area = self.width * self.height
+      return area
+
+
+  def get_perimeter(self):
+      perimeter = (2 * self.width) + (2 * self.height)
+      return perimeter
+
+
+  def get_diagonal(self):
+      diagonal = ((self.width**2 + self.height**2) **.5)
+      return diagonal
+
+
+  def get_picture(self):
+      if self.width > 50 or self.height > 50:
+          return "Too big for picture."
+      else:
+          width = (self.width * "*") + "\n"
+          picture = self.height * width
+          return picture
+
+
+  def get_amount_inside(self, shape):
+      w_int_qty = self.width // shape.width #=> comparing the polygons width
+      h_int_qty = self.height // shape.height #=> comparing the polygons height
+      #number of times the shape fits in the rectangle:
+      qty_pol_inside = w_int_qty * h_int_qty
+      return qty_pol_inside
+
+class Square(Rectangle):
+  def __init__(self, side):
+      self.side = side
+      width = self.side
+      height = self.side
+      super().__init__(width, height)
+
+
+  def __str__(self):
+      return f"Square(side={self.side})"
+
+
+  def set_side(self, new_side):
+      self.side = new_side
+      self.width = new_side
+      self.height = new_side
+
+
+  def set_width (self, new_width):
+      self.set_side(new_width)
+
+
+  def set_height(self, new_height):
+      self.set_side(new_height)
+    
